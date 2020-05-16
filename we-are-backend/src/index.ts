4f4cd3c3a2c5cc44ae3cd9280c13bb73a24aea38
Stephan Tentrup <stephan.tentrup@codecentric.de>
2020-05-16 14:11:01 +0200

    Parse body as JSON

--- we-are-backend/src/index.ts
+++ we-are-backend/src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import { PostEndpoints } from './api/PostEndpoints'
 import { PostService } from './services/PostService'
+import bodyParser from 'body-parser'
 
 const app = express()
 const port = 3000
+
+app.use(bodyParser.json())
 
 const postService = new PostService()
 const postEndpoints = new PostEndpoints(postService)
--- we-are-backend/src/services/PostService.ts
+++ we-are-backend/src/services/PostService.ts
@@ -3,7 +3,7 @@ import { Post } from '../model/Post';
 export class PostService {
 
     public createPost(post: Post) {
-        console.log('Received a new post: ' + post)
+        console.log(`Received a new post: ${JSON.stringify(post)}`)
     }
 
     public getPosts() {
