b616646ff1990ebf1abd9402b4e863cbace7be7e
Stephan Tentrup <stephan.tentrup@codecentric.de>
2020-05-16 21:05:47 +0200

    allow specific headers

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,24 @@ import cors from 'cors'
 
 const app = express()
 
-app.use(cors())
+var allowedOrigins = [
+    'http://localhost:3000',
+    'https://we-are-pride.herokuapp.com'
+];
+app.use(cors({
+    origin: function(origin, callback){
+        // allow requests with no origin
+        // (like mobile apps or curl requests)
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
+
 app.use(bodyParser.json())
 
 const postRepository = new PostRepository(process.env.DATABASE_URL)
