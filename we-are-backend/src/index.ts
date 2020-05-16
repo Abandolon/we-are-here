51ed9c9b82e0705e25d9e51cfa16dd96c520f70b
Stephan Tentrup <stephan.tentrup@codecentric.de>
2020-05-16 15:41:20 +0200

    get posts

--- we-are-backend/src/index.ts
+++ we-are-backend/src/index.ts
@@ -2,16 +2,18 @@ import express from 'express'
 import { PostEndpoints } from './api/PostEndpoints'
 import { PostService } from './services/PostService'
 import bodyParser from 'body-parser'
+import { PostRepository } from './db/PostRepository'
 
 const app = express()
 const port = 3000
 
 app.use(bodyParser.json())
 
-const postService = new PostService()
+const postRepository = new PostRepository()
+const postService = new PostService(postRepository)
 const postEndpoints = new PostEndpoints(postService)
 
 app.post('/post', postEndpoints.createPost)
-app.get('/', (_req, res) => res.send('Hello World!'))
+app.get('/posts', postEndpoints.getPosts)
 
-app.listen(port, () => console.log(`App listening at port ${ port }`))
+app.listen(port, () => console.log(`We are here backend is listening at port ${ port }`))
--- we-are-backend/src/services/PostService.ts
+++ we-are-backend/src/services/PostService.ts
@@ -1,12 +1,20 @@
 import { Post } from '../model/Post';
+import { PostRepository } from '../db/PostRepository'
 
 export class PostService {
 
-    public createPost(post: Post) {
-        console.log(`Received a new post: ${JSON.stringify(post)}`)
+    constructor(
+        private repository: PostRepository
+    ) {
     }
 
-    public getPosts() {
-        // TODO
+    public createPost(post: Post) {
+        console.log(`Received a new post: ${JSON.stringify(post)}`)
+        this.repository.create(post)
+    }
+
+    public getPosts(limit?: number) {
+        const posts = this.repository.get(limit)
+        return posts
     }
 }
